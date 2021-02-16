d762514b744690c324a1f7fbbdd0e05753ad01e0
AndreasOeS <mail.andreas.s@gmail.com>
2021-02-16 11:05:23 +0100

    Gået igang med Bluetooth api

--- app/src/main/java/com/example/wegoeco/MainActivity.java
+++ app/src/main/java/com/example/wegoeco/MainActivity.java
@@ -1,24 +1,97 @@
 package com.example.wegoeco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //public static final String BLUETOOTH;
+    //public static final String ACCESS_FINE_LOCATION;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+    }
+
+
+    public void connectBlue(){
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            System.out.println("Device doesn't support Bluetooth");
+        }
+        else{
+            if(!bluetoothAdapter.isEnabled()){
+
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            }
+        }
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(this.getBaseContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, PERMISSION_CODE);
+            }
+        }
+
+
+
+    }
+
+    public void startActivityForResult (Intent intent, int requestCode){
+        if (requestCode == PERMISSION_CODE){
+
+        }
+
+    }
+
+
+    public void onActivityResult (int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            if(resultCode == RESULT_OK){
+
+
+            }
+            if (resultCode == RESULT_CANCELED){
+
+            }
+        }
+
+
+    }
+
+
+    public void writeToDatabase(){
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
-
     }
+
+
 }
