71b57f849ca5e34f02468a12689e40c61243b5c1
AndreasOeS <43959623+AndreasOeS@users.noreply.github.com>
2021-04-20 17:38:23 +0200

    Trips kan nu oploades til firebase

--- app/src/main/java/com/example/wegoeco/MainActivity.java
+++ app/src/main/java/com/example/wegoeco/MainActivity.java
@@ -11,6 +11,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -19,14 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
+        database = FirebaseDatabase.getInstance();
 
         //myRef.setValue("Hello, World!");
-//        Intent intent = new Intent(this, BluetoothActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(this, BluetoothActivity.class);
+        startActivity(intent);
 
-        Trip trip = new Trip(1,2,3,4,5,6);
+
+        //Trip trip = new Trip(1,2,3,4,5,6);
 
 //        trip.setStartTime((int) System.currentTimeMillis()/1000);
 //        trip.setEndTime((int) System.currentTimeMillis()/1000);
@@ -42,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
         //Trip uploadTrip = new Trip(1,2,3,4,5);
 
 
-        Firebase firebase = new Firebase();
-        firebase.upload(trip);
+//        Firebase firebase = new Firebase();
+//        firebase.upload(trip);
+        //database.getReference().child("message").push().setValue(trip);
+//        myRef.push().setValue(trip);
 
 
     }
