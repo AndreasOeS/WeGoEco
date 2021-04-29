7ae49676b7aed7d308a1afb0776e9cbd45494915
icodenaked90 <43744105+icodenaked90@users.noreply.github.com>
2021-04-29 12:17:58 +0200

    it works ?

--- app/src/main/java/com/example/wegoeco/Datastream.java
+++ app/src/main/java/com/example/wegoeco/Datastream.java
@@ -220,7 +220,7 @@ public class Datastream extends Thread {
 
 
     public void setUp() throws IOException {
-
+            sendCommand(atStop);
             for (int i = 0;i < commands.length;i++){
                 sendCommand(commands[i]);
             }
